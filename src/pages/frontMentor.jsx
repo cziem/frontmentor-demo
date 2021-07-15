cc1b096b6d8a2644c63b8beaa20d85efbd8ddc76
George Favour C <phavorsparks@gmail.com>
2021-07-15 20:44:40 +0100

    style the header

--- src/pages/frontMentor.jsx
+++ src/pages/frontMentor.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoBulb } from "react-icons/io5";
 import Card from "../components/Card";
 import { suggestionsData } from "../data/suggestions.data";
 import "./frontMentor.scss";
@@ -7,15 +9,24 @@ const FrontMentor = () => {
   return (
     <div className="frontMentor__main">
       <header>
-        <div className="suggestion">
-          <p>
-            <b>6 Suggestions</b>
-          </p>
+        <div className="info__metrics">
+          <div className="suggestion">
+            <IoBulb />
+            <p>
+              <b>6 Suggestions</b>
+            </p>
+          </div>
+
+          <div className="sortBy">
+            <span>Sort by:</span>
+            <b>
+              Most Upvotes <MdKeyboardArrowDown />
+            </b>
+          </div>
         </div>
 
-        <div className="sortBy">
-          <span>Sort by:</span>
-          <b>Most Upvotes</b>
+        <div className="cta">
+          <button>add feedback</button>
         </div>
       </header>
 
